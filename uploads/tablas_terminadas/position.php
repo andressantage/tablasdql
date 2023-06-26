585958b93157402a8872bc089caac0efec3d4cb9
andressantage <ingeniero.andres.santana@gmail.com>
2023-06-26 15:10:11 -0500

    feat: :sparkles: 7 tabla: position
    realizado

--- uploads/tablas_terminadas/position.php
+++ uploads/tablas_terminadas/position.php
@@ -4,7 +4,7 @@
 
     $router->get("/camper", function() {
         $cox= new \App\connect();
-        $res=$cox->con->prepare("SELECT * FROM tablass");
+        $res=$cox->con->prepare("SELECT * FROM position");
         $res->execute();
         $res=$res->fetchAll(\PDO::FETCH_ASSOC);
         echo json_encode($res);
@@ -13,7 +13,7 @@
     $router->put('/camper', function() {
         $_DATA=json_decode(file_get_contents("php://input"),true);
         $cox= new \App\connect();
-        $res=$cox->con->prepare("UPDATE tablass SET nombre = :NOMBRE WHERE id=:CEDULA");
+        $res=$cox->con->prepare("UPDATE position SET name_position = :NOMBRE WHERE id=:CEDULA");
         $res->bindValue("NOMBRE",$_DATA["nom"]);
         $res->bindValue("CEDULA",$_DATA["id"]);
         $res->execute();
@@ -21,14 +21,14 @@
         echo json_encode($res);
     });
     /* {
-        "id": 4,
-        "nom": ""
+        "id": 9,
+        "nom": "Scrum"
     } */
 
     $router->delete("/camper", function(){
         $_DATA = json_decode(file_get_contents("php://input"), true);
         $cox = new \App\connect();
-        $res = $cox->con->prepare("DELETE FROM tablass WHERE id = :ID"); 
+        $res = $cox->con->prepare("DELETE FROM position WHERE id = :ID"); 
         $res->bindValue("ID", $_DATA["id"]);
         $res->execute();
         $res = $res->rowCount();//es para obtener el número de filas afectadas por la actualización
@@ -38,14 +38,16 @@
     $router->post("/camper", function(){
         $_DATA = json_decode(file_get_contents("php://input"), true); 
         $cox = new \App\connect();
-        $res = $cox->con->prepare("INSERT INTO tablass (nombre) VALUES (:NOMBRE)");
+        $res = $cox->con->prepare("INSERT INTO position (name_position,arl) VALUES (:NOMBRE,:r)");
         $res->bindValue("NOMBRE", $_DATA["nom"]); 
+        $res->bindValue("r", $_DATA["r"]); 
         $res->execute();
         $resi=$res->rowCount(); 
         echo $resi;
     });
     /* {
-        "nom": ""
+        "nom": "Lider",
+        "r": "Emdisalud"
     } */
 
     $router->run();
